56271b524f1096ba047dd56ca421c3ce3bbb061f
DMaroo <dhruvmaru007@gmail.com>
2021-07-19 18:50:50 +0530

    Intermediate commit
    Still doesn't work, but this seems more likely to be the right solution

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,10 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <image_geometry/pinhole_camera_model.h>
 #include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/Vector3.h>
 #include <string>
 #include "detect_potholes.hpp"
 
@@ -51,11 +54,11 @@ public:
 
 		m_cam_model.fromCameraInfo(info_msg);
 
-		geometry_msgs::TransformStamped cam2ground;
+		geometry_msgs::TransformStamped cam2odom;
 
 		try
 		{
-			cam2ground = m_tf_buffer.lookupTransform("base_link", m_cam_model.tfFrame(), ros::Time(0));
+			cam2odom = m_tf_buffer.lookupTransform(m_cam_model.tfFrame(), "odom", ros::Time(0));
 		}
 		catch (tf2::TransformException &exc)
 		{
@@ -64,43 +67,69 @@ public:
 			return;
 		}
 
-		cv::Point3d unit_vec;
+		cv::Point3d direction_vec;
+		geometry_msgs::Point pos_cam, pos_odom;
+		geometry_msgs::Vector3 vec_cam, vec_odom;
+
+		pos_cam.x = 0;
+		pos_cam.y = 0;
+		pos_cam.z = 0;
 
 		int count = 1;
 
 		for (auto &pixel : cam_result)
 		{
-			unit_vec = m_cam_model.projectPixelTo3dRay(pixel);
+			direction_vec = m_cam_model.projectPixelTo3dRay(pixel);
 
-			double norm_x = unit_vec.x, norm_y = unit_vec.y, norm_z = unit_vec.z, norm;
+			vec_cam.x = direction_vec.x;
+			vec_cam.y = direction_vec.z;
+			vec_cam.z = -direction_vec.y;
 
-			norm = sqrt(norm_x*norm_x + norm_y*norm_y + norm_z*norm_z);
+			double norm = sqrt(vec_cam.x*vec_cam.x + vec_cam.y*vec_cam.y + vec_cam.z*vec_cam.z);
 
-			norm_x /= norm;
-			norm_y /= norm;
-			norm_z /= norm;
+			vec_cam.x /= norm;
+			vec_cam.y /= norm;
+			vec_cam.z /= norm;
 
-			tf2::Quaternion vec_q, tf_q, conj_tf_q;
+			tf2::doTransform<geometry_msgs::Point>(pos_cam, pos_odom, cam2odom);
+			tf2::doTransform<geometry_msgs::Vector3>(vec_cam, vec_odom, cam2odom);
 
-			vec_q.setValue(norm_x, norm_y, norm_z, 0.0);
+			// double norm_x = unit_vec.x, norm_y = unit_vec.y, norm_z = unit_vec.z, norm;
 
-			auto temp = cam2ground.transform.rotation;
+			// norm = sqrt(norm_x*norm_x + norm_y*norm_y + norm_z*norm_z);
 
-			tf_q.setValue(temp.x, temp.y, temp.z, temp.w);
-			tf_q = tf_q.inverse();
+			// norm_x /= norm;
+			// norm_y /= norm;
+			// norm_z /= norm;
 
-			conj_tf_q.setValue(tf_q.x(), -tf_q.y(), -tf_q.z(), -tf_q.w());
+			// tf2::Quaternion vec_q, tf_q, conj_tf_q_inv;
 
-			vec_q = tf_q*vec_q*conj_tf_q;
+			// vec_q.setValue(norm_x, norm_z, -norm_y, 0.0);
 
-			double lambda = -cam2ground.transform.translation.z/vec_q.z();
+			// auto temp = cam2odom.transform.rotation;
 
-			double x = cam2ground.transform.translation.x + lambda*vec_q.x();
-			double y = cam2ground.transform.translation.y + lambda*vec_q.y();
+			// tf_q.setValue(temp.x, temp.y, temp.z, temp.w);
+			// tf2::Quaternion tf_q_inv = tf_q.inverse();
 
-			ROS_INFO("Pothole %d: (%lf, %lf) | Vector: %lf %lf %lf", count, x, y, vec_q.x(), vec_q.y(), vec_q.z());
+			// conj_tf_q_inv.setValue(tf_q_inv.x(), -tf_q_inv.y(), -tf_q_inv.z(), -tf_q_inv.w());
 
-			ros::Duration(0.1).sleep();
+			// tf2::Quaternion result_q = -conj_tf_q_inv*vec_q*tf_q_inv;
+
+			norm = sqrt(vec_odom.x*vec_odom.x + vec_odom.y*vec_odom.y + vec_odom.z*vec_odom.z);
+
+			vec_odom.x /= norm;
+			vec_odom.y /= norm;
+			vec_odom.z /= norm;
+
+			double lambda = -pos_odom.z/vec_odom.z;
+
+			double x = pos_odom.x + lambda*vec_odom.x;
+			double y = pos_odom.y + lambda*vec_odom.y;
+
+			ROS_INFO("Pothole %d: (%lf, %lf) | vec_odom: (%lf, %lf, %lf) | pos_odom: (%lf, %lf, %lf)", count, x, y, vec_odom.x, vec_odom.y, vec_odom.z, pos_odom.x, pos_odom.y, pos_odom.z);
+
+			ros::Duration(0.1)
+				.sleep();
 
 			count++;
 		}
