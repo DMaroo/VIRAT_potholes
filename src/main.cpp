0abd7309e8223352dd0340f60c95ac2738d51a43
DMaroo <dhruvmaru007@gmail.com>
2021-07-21 14:49:25 +0530

    Corrected vec_cam and pos_odom

--- src/main.cpp
+++ src/main.cpp
@@ -21,11 +21,14 @@ class ImageConverter
 	image_transport::CameraSubscriber m_image_sub;
 	image_geometry::PinholeCameraModel m_cam_model;
 	tf2_ros::Buffer m_tf_buffer;
+	tf2_ros::Buffer m_tf_buffer1;
 	tf2_ros::TransformListener m_tf_listener;
+	tf2_ros::TransformListener m_tf_listener1;
 
 public:
 	ImageConverter() : m_it(m_nh),
-					   m_tf_listener(m_tf_buffer)
+					   m_tf_listener(m_tf_buffer),
+					   m_tf_listener1(m_tf_buffer1)
 	{
 		m_image_sub = m_it.subscribeCamera("/virat/camera_top/image_raw", 1, &ImageConverter::image_cvb, this);
 	}
@@ -52,11 +55,12 @@ public:
 
 		m_cam_model.fromCameraInfo(info_msg);
 
-		geometry_msgs::TransformStamped cam2odom;
+		geometry_msgs::TransformStamped odom2cam, cam2odom;
 
 		try
 		{
-			cam2odom = m_tf_buffer.lookupTransform(m_cam_model.tfFrame(), "odom", ros::Time(0));
+			odom2cam = m_tf_buffer.lookupTransform("odom", m_cam_model.tfFrame(), ros::Time(0));
+			cam2odom = m_tf_buffer1.lookupTransform(m_cam_model.tfFrame(), "odom", ros::Time(0));
 		}
 		catch (tf2::TransformException &exc)
 		{
@@ -79,8 +83,8 @@ public:
 		{
 			direction_vec = m_cam_model.projectPixelTo3dRay(pixel);
 
-			vec_cam.x = direction_vec.x;
-			vec_cam.y = direction_vec.z;
+			vec_cam.x = direction_vec.z;
+			vec_cam.y = -direction_vec.x;
 			vec_cam.z = -direction_vec.y;
 
 			double norm = sqrt(vec_cam.x * vec_cam.x + vec_cam.y * vec_cam.y + vec_cam.z * vec_cam.z);
@@ -89,7 +93,7 @@ public:
 			vec_cam.y /= norm;
 			vec_cam.z /= norm;
 
-			tf2::doTransform<geometry_msgs::Point>(pos_cam, pos_odom, cam2odom);
+			tf2::doTransform<geometry_msgs::Point>(pos_cam, pos_odom, odom2cam);
 			tf2::doTransform<geometry_msgs::Vector3>(vec_cam, vec_odom, cam2odom);
 
 			// double norm_x = unit_vec.x, norm_y = unit_vec.y, norm_z = unit_vec.z, norm;
@@ -104,7 +108,7 @@ public:
 
 			// vec_q.setValue(norm_x, norm_z, -norm_y, 0.0);
 
-			// auto temp = cam2odom.transform.rotation;
+			// auto temp = odom2cam.transform.rotation;
 
 			// tf_q.setValue(temp.x, temp.y, temp.z, temp.w);
 			// tf2::Quaternion tf_q_inv = tf_q.inverse();
@@ -124,7 +128,7 @@ public:
 			double x = pos_odom.x + lambda * vec_odom.x;
 			double y = pos_odom.y + lambda * vec_odom.y;
 
-			ROS_INFO("Pothole %d: (%lf, %lf) | vec_odom: (%lf, %lf, %lf) | pos_odom: (%lf, %lf, %lf)", count, x, y, vec_odom.x, vec_odom.y, vec_odom.z, pos_odom.x, pos_odom.y, pos_odom.z);
+			ROS_INFO("Pothole %d: (%lf, %lf) | pos_odom: (%lf, %lf, %lf)", count, x, y, pos_odom.x, pos_odom.y, pos_odom.z);
 
 			ros::Duration(0.1).sleep();
 
